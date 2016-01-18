b668abbbf396e1068579056ff6c53f7bbed5fa10
hibreez <4ngus.whitehead@gmail.com>
2016-01-18 03:16:34 +0000

    Adds particle effect to dandy sprite when ascending and switches to desert platform/background images.

--- src/app.js
+++ src/app.js
@@ -7,26 +7,34 @@ var game = new Phaser.Game(
 var game, cursors, player, platforms, ground, ledge;
 
 function preload() {
-  game.load.image('sky', 'assets/sky.png');
-  game.load.image('ground', 'assets/platform.png');
-  game.load.spritesheet('player', 'assets/dandy.png');
+  game.load.image('sky', 'assets/BG.png');
+  game.load.image('player', 'assets/dandy.png');
+  game.load.image('particle', 'assets/white-smoke.png');
+  game.load.image('platform', 'assets/platform.png');
 }
 
 function create() {
   game.physics.startSystem(Phaser.Physics.ARCADE);
   game.add.sprite(0, 0, 'sky');
 
-  player = game.add.sprite(32, game.world.height - 150, 'player');
-  game.physics.arcade.enable(player);
+  emitter = game.add.emitter(game.world.centerX, 500, 400);
+  emitter.makeParticles('particle');
+  emitter.setAlpha(0.1, 0.25, 3000);
+  emitter.setScale(0.04, 0.5, 0.04, 0.2, 6000, Phaser.Easing.Quintic.Out);
+  emitter.gravity = -20;
+  emitter.start(false, 2500, 100);
 
   platforms = game.add.group();
   platforms.enableBody = true;
-  ground = platforms.create(0, game.world.height - 64, 'ground');
-  ground.scale.setTo(2, 2);
-  ledge = platforms.create(400, 400, 'ground');
-  ledge = platforms.create(-150, 250, 'ground');
+  ground = platforms.create(-60, game.world.height - 24, 'platform');
+  ground.scale.setTo(3, 3);
+  ledge = platforms.create(400, 400, 'platform');
+  ledge = platforms.create(-150, 250, 'platform');
   platforms.setAll('body.immovable', true);
  
+  player = game.add.sprite(32, game.world.height - 150, 'player');
+  game.physics.arcade.enable(player);
+
   cursors = game.input.keyboard.createCursorKeys();
 }
 
@@ -34,6 +42,7 @@ function update () {
   game.physics.arcade.collide(player, platforms);
   processControls(player);
   applyPlayerDrag(player);
+  emitTrail();
   game.world.wrap(player, 20);
 }
 
@@ -69,5 +78,19 @@ function applyPlayerDrag(player) {
   }
   else {
     player.body.drag.x = 50;
+  }
+}
+
+function emitTrail() {
+  if (cursors.up.isDown) {
+    var px = player.body.velocity.x * -0.2;
+    var py = player.body.velocity.y * -0.2;
+    emitter.minParticleSpeed.set(px, py);
+    emitter.maxParticleSpeed.set(px, py);
+    emitter.emitX = player.x + 28;
+    emitter.emitY = player.y + 10;
+    emitter.on = true;
+  } else {
+    emitter.on = false;
   }
 }
